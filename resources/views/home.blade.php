52e935b294eb1e9f93e18d4ea6afc96b7617b72b
Dzikri <dikrilahi@gmail.com>
2026-06-06 15:30:17 +0700

    penambahan css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,44 +2,51 @@
 <html>
 <head>
     <title>Home - Laravel</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
-    <h1>Selamat Datang di Website Pengenalan Laravel</h1>
+    <div class="container">
 
-    <p>
-        Website ini dibuat sebagai tugas pembelajaran Framework Laravel.
-        Melalui website ini, pengguna dapat mengenal apa itu Laravel,
-        fungsi Laravel, serta manfaatnya dalam pengembangan aplikasi web.
-    </p>
+        <h1>Selamat Datang di Website Pengenalan Laravel</h1>
 
-    <h2>Tentang Website</h2>
-    <p>
-        Website ini terdiri dari beberapa halaman, yaitu Home, About, dan Contact.
-        Setiap halaman memiliki informasi yang berbeda untuk membantu pengguna
-        memahami framework Laravel dengan lebih mudah.
-    </p>
+        <p>
+            Website ini dibuat sebagai tugas pembelajaran Framework Laravel.
+            Melalui website ini, pengguna dapat mengenal apa itu Laravel,
+            fungsi Laravel, serta manfaatnya dalam pengembangan aplikasi web.
+        </p>
 
-    <h2>Tujuan Pembuatan Website</h2>
-    <ul>
-        <li>Memenuhi tugas mata kuliah Pemrograman Web.</li>
-        <li>Mempelajari penggunaan Framework Laravel.</li>
-        <li>Memahami konsep Route, Controller, dan View.</li>
-        <li>Melatih kerja sama tim dalam pengembangan website.</li>
-    </ul>
+        <h2>Tentang Website</h2>
 
-    <h2>Fitur Website</h2>
-    <ul>
-        <li>Halaman Home sebagai halaman utama.</li>
-        <li>Halaman About yang berisi pengenalan Laravel.</li>
-        <li>Halaman Contact yang berisi informasi kelompok.</li>
-    </ul>
+        <p>
+            Website ini terdiri dari beberapa halaman, yaitu Home, About, dan Contact.
+            Setiap halaman memiliki informasi yang berbeda untuk membantu pengguna
+            memahami framework Laravel dengan lebih mudah.
+        </p>
 
-    <hr>
+        <h2>Tujuan Pembuatan Website</h2>
 
-    <p>
-        Gunakan menu navigasi untuk berpindah ke halaman About dan Contact.
-    </p>
+        <ul>
+            <li>Memenuhi tugas mata kuliah Pemrograman Web.</li>
+            <li>Mempelajari penggunaan Framework Laravel.</li>
+            <li>Memahami konsep Route, Controller, dan View.</li>
+            <li>Melatih kerja sama tim dalam pengembangan website.</li>
+        </ul>
+
+        <h2>Fitur Website</h2>
+
+        <ul>
+            <li>Halaman Home sebagai halaman utama.</li>
+            <li>Halaman About yang berisi pengenalan Laravel.</li>
+            <li>Halaman Contact yang berisi informasi kelompok.</li>
+        </ul>
+
+        <div class="footer">
+            <p>&copy; 2026 Website Pengenalan Laravel</p>
+        </div>
+
+    </div>
 
 </body>
 </html>
